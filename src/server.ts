bf322f7b8bdfbb84879eaf597a00bd50d3e1d8ae
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-22 16:36:05 +0900

    corsの設定を変更

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,7 @@ const app = express()
 
 const PORT = 4000
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  }),
-)
+app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
